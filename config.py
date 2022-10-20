5a502b929b1eb62eaa724e69a13fc8098b059792
GeliosI <mishalat@yandex.ru>
2022-10-20 16:29:13 +0300

    Rename VkTokensList (--> VkConfig), add group_id

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from tokenize import group
 from dotenv import find_dotenv, load_dotenv
 
 
@@ -17,7 +18,8 @@ class DatabaseConfig(Config):
         self.echo = _str_to_bool_convert(os.getenv('DB_ECHO'))
 
 
-class VkTokensList(Config):
+class VkConfig(Config):
+    group_id = os.getenv('VK_GROUP_ID')
     group_token = os.getenv('VK_GROUP_TOKEN')
     user_token = os.getenv('VK_USER_TOKEN')
 
