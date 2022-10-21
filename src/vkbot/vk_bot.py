11fa9b98263636d30b4ffae434293092805dd39f
mishalat@yandex.ru <mishalat@yandex.ru>
2022-10-21 10:18:10 +0300

    + Добавление и показ избранного

--- src/vkbot/vk_bot.py
+++ src/vkbot/vk_bot.py
@@ -12,6 +12,8 @@ class VkBot():
         DataClassesDBI.dbi = DatabaseInterface(db_config)      
         self._create_group_session(vk_config.group_token, vk_config.group_id)
         self._create_user_session(vk_config.user_token)
+        self._init_current_user_param()
+        self._init_menu()
 
     def _create_group_session(self, group_token, group_id):
         """ Инициирует сессию с токеном группы  """
@@ -20,12 +22,23 @@ class VkBot():
         self.longpoll = VkBotLongPoll(self.group_session, group_id)
         self.group_api = self.group_session.get_api()
 
-
     def _create_user_session(self, token):
         """ Инициирует сессию с токеном пользователя """
 
         self.user_session = vk_api.VkApi(token=token)
         self.user_api = self.user_session.get_api()
+
+    def _init_current_user_param(self):
+        self.city = None
+        self.sex = None
+        self.birth_year = None
+        self.targets = None
+        self.curent_target = None
+
+    def _init_menu(self):
+        self.start_menu = self.get_start_menu()
+        self.restart_menu = self.get_restart_menu()        
+        self.main_menu = self.get_main_menu()
 
     def get_start_menu(self):
         """ Начальное меню """
@@ -143,23 +156,18 @@ class VkBot():
                 target.photos.append(Photo(photo_id, target_vk_id, photo_link))
             targets.append(target)
 
-        # for target in targets:
-        #     target.add_favorite(client_vk_id)
-
-        # for fav in targets.get_favorites():
-        #     print(fav.first_name)
         return iter(targets)
 
+    def get_opposite_sex(self, sex):
+        sex = sex - 1 if sex == 2 else sex + 1
+        return sex
+        
     def search_start_state(self, event, current_user):
-        start_menu = self.get_start_menu()
-        restart_menu = self.get_restart_menu()
-
         user_info = self.get_user_info(event.obj.message['from_id'], fields='bdate, sex, city')
 
         if 'city' in user_info:
             self.city = user_info['city']['id']
         else:
-            self.city = None
             self.send_message(current_user, 'Кажется, у вас не указан город!')
 
         if user_info['sex'] == 1:
@@ -172,35 +180,52 @@ class VkBot():
         if 'bdate' in user_info and len(user_info['bdate'].split('.')) == 3:
             self.birth_year = user_info['bdate'].split('.')[2]
         else:
-            self.birth_year = None
             self.send_message(current_user, 'Кажется, у вас не указан год рождения!')                     
 
-        if self.city is None or self == 0 or self.birth_year is None:
-            self.send_message(current_user, 'Измените настройки профиля и перезапустите бота!', keyboard=restart_menu)
+        if not (self.city and self.sex and self.birth_year):
+            self.send_message(current_user, 'Измените настройки профиля и перезапустите бота!', keyboard=self.restart_menu)
         else:
             self.send_message(current_user, f'Ваш город (city_id): {self.city}')
             self.send_message(current_user, f'Ваш пол: {"мужской" if self.sex == 1 else "женский"}')
             self.send_message(current_user, f'Ваш год рождения: {self.birth_year}')
-            self.send_message(current_user, 'Давай знакомиться? Начни поиск!', keyboard=start_menu)
+            self.send_message(current_user, 'Давай знакомиться? Начни поиск!', keyboard=self.start_menu)
             return True
 
     def search_active_state(self, current_user):
-        main_menu = self.get_main_menu()  
-        self.send_message(current_user, 'Поиск запущен...', keyboard=main_menu)
+        self.send_message(current_user, 'Поиск запущен...')
         users = self.find_users(self.birth_year, self.sex, self.city, fields='bdate, sex, city')
         self.targets = self.create_obj(current_user, imitation_users_list_from_api=users)
         target = next(self.targets)
+        self.curent_target = target
         message = f'{target.first_name} {target.last_name}\n{target.url}'
         attachment = ",".join([photo.photo_link for photo in target.photos])
+        self.send_message(current_user, message, attachment, keyboard=self.main_menu)
+
+    def search_continue_state(self, current_user):
+        self.send_message(current_user, 'Продолжаю поиск...', keyboard=self.main_menu)
+        target = next(self.targets)
+        self.curent_target = target
+        message = f'{target.first_name} {target.last_name}\n{target.url}'
+        attachment = ",".join([photo.photo_link for photo in target.photos])                    
         self.send_message(current_user, message, attachment)
+
+    def add_fav_state(self, current_user):
+        self.curent_target.add_favorite(current_user)
+        self.send_message(current_user, 'Данные обновлены', keyboard=self.main_menu)
+
+    def show_fav_state(self, current_user):
+        self.send_message(current_user, 'Избранное:', keyboard=self.main_menu)
+        for fav in self.targets.get_favorites():
+            message = f'{fav.first_name} {fav.last_name}\n{fav.url}'
+            attachment = ",".join([photo.photo_link for photo in fav.photos])
+            self.send_message(current_user, message, attachment, keyboard=self.main_menu)
 
     def _listener(self):
         """ https://vk.com/dev/bots_longpoll """
-
+        
         search_start_flag = False
         stop_menu = self.get_stop_menu()
-        main_menu = self.get_main_menu() 
-        
+
         for event in self.longpoll.listen():            
             if event.type == VkBotEventType.MESSAGE_NEW:
                 current_user = event.obj.message['from_id']
@@ -211,18 +236,13 @@ class VkBot():
                 elif current_message == 'Начать поиск' and search_start_flag:
                     self.search_active_state(current_user)
                 elif current_message == 'Продолжить поиск' and search_start_flag:
-                    self.send_message(current_user, 'Продолжаю поиск...', keyboard=main_menu)
-                    target = next(self.targets)
-                    message = f'{target.first_name} {target.last_name}\n{target.url}'
-                    attachment = ",".join([photo.photo_link for photo in target.photos])                    
-                    self.send_message(current_user, message, attachment)
+                    self.search_continue_state(current_user)
                 elif current_message == 'Добавить в избранное' and search_start_flag:
-                    self.send_message(current_user, 'Данные обновлены', keyboard=main_menu)
+                    self.add_fav_state(current_user)
                 elif current_message == 'Показать избранное' and search_start_flag:
-                    self.send_message(current_user, 'Избранное:', keyboard=main_menu)
+                    self.show_fav_state(current_user)
                 elif current_message == 'Остановить бота':
-                    self.send_message(current_user, 'Бот остановлен', keyboard=stop_menu)                    
-
+                    self.send_message(current_user, 'Бот остановлен', keyboard=stop_menu)
             else:
                 print(event.type)
                 print()
