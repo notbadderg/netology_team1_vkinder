60c913104402ae22e5cdfa69fb108942dada683c
mishalat@yandex.ru <mishalat@yandex.ru>
2022-10-21 19:11:34 +0300

    add stop state

--- src/vkbot/vk_bot.py
+++ src/vkbot/vk_bot.py
@@ -130,6 +130,8 @@ class VkBot():
         return resp
 
     def get_photo_link(self, user_id):
+        """ Получить ссылку на фото по id пользователя """
+        
         photo = self.get_photos_by_owner_id(user_id)
         result = []
 
@@ -220,12 +222,14 @@ class VkBot():
             attachment = ",".join([photo.photo_link for photo in fav.photos])
             self.send_message(current_user, message, attachment, keyboard=self.main_menu)
 
+    def stop_state(self, current_user):
+        self.send_message(current_user, 'Бот остановлен', keyboard=self.stop_menu)
+
     def _listener(self):
         """ https://vk.com/dev/bots_longpoll """
         
         search_start_flag = False
-        stop_menu = self.get_stop_menu()
-
+        
         for event in self.longpoll.listen():            
             if event.type == VkBotEventType.MESSAGE_NEW:
                 current_user = event.obj.message['from_id']
@@ -242,65 +246,8 @@ class VkBot():
                 elif current_message == 'Показать избранное' and search_start_flag:
                     self.show_fav_state(current_user)
                 elif current_message == 'Остановить бота':
-                    self.send_message(current_user, 'Бот остановлен', keyboard=stop_menu)
-            else:
-                print(event.type)
-                print()
+                    self.stop_state(current_user)
+
 
     def start(self):
         self._listener()
-        #########
-
-
-
-
-
-
-
-
-
-        #FOR QUICK DB TESTING
-        def test_db():
-            # ##FIXTURES:
-            client_vk_id = 110
-            imitation_users_list_from_api = [{'id': 200, 'first_name': 'Daria99', 'last_name': 'Lavrova99'}]
-            imitation_user_photos_from_api = [{'id': 2002, 'owner_id': 200},
-                                              {'id': 2003, 'owner_id': 200}]
-
-            # обрабатываем полученную инфу с апи чтобы сформировать объекты
-            targets = TargetsList(client_vk_id=client_vk_id)
-            for user in imitation_users_list_from_api:
-                target = Target(user['id'],
-                                user['first_name'],
-                                user['last_name'],
-                                'sample_url')
-
-                for photo in imitation_user_photos_from_api:
-                    target.photos.append(Photo(photo_id=photo['id'],
-                                               target_vk_id=photo['owner_id'],
-                                               photo_link='sample_link'))
-                targets.append(target)
-
-            # показываем по очереди фото из targets клиенту:
-            # через next или через цикл
-            for target in targets:
-                print(target.first_name)
-
-            # если хотим добавить текущую цель в избранное (сразу в бд) надо лишь прописать:
-                result = target.add_favorite(client_vk_id=client_vk_id)
-                if result:
-                    print('Добавлено')
-                else:
-                    print('Не добавлено или уже добавлено')
-
-            # если хотим получить список избранного, также через TargetsList работаем:
-            favorites = targets.get_favorites()
-
-            # показываем точно также как и в начале
-            for favorite in favorites:
-                print(favorite.first_name)
-                print(favorite.photos)
-                # достаем фотки:
-                for photo in favorite.photos:
-                    print(photo.photo_id)
-        # test_db()
